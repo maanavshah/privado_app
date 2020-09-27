c51b38e311a25592876ad5a2c29e28981c9a1a3e
maanav <shah.maanav.07@gmail.com>
2020-09-27 17:27:47 +0530

    Add routes url

--- PrivadoApp/urls.py
+++ PrivadoApp/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from privado_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('te/customer/<str:customer_id>/templates', views.vw_templates),
 ]
